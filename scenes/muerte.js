8fbdebe2b580901de3616ad2c5d87f56209826c2
Sergio Duran <sergio@tiene.info>
2020-02-08 17:59:48 -0600

    Reducir el texto de la muerte a un solo mensaje

--- scenes/muerte.js
+++ scenes/muerte.js
@@ -3,6 +3,5 @@ const Scene = require('telegraf/scenes/base')
 module.exports = new Scene('muerte')
   .enter(async (ctx) => {
     console.log('Cuarto inicial:', ctx.update.message.from.first_name)
-    await ctx.basicReply(500, 'He muerto, gracias por nada')
-    await ctx.basicReply(500, 'Para volver a empezar, escribe /start')
+    await ctx.basicReply(0, 'He muerto, gracias por nada\nPara volver a empezar, escribe /start')
   })
